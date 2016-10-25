9e716d5bfa6e3fe05fa46067159c0c11411eb67a
Oliver <oliver.henry.walters@gmail.com>
2016-10-25 22:18:23 +1100

    Debug handler code

--- logging/debug_handler.cpp
+++ logging/debug_handler.cpp
@@ -1,1 +1,11 @@
+#include "debug_handler.h"
+
+void AndromedaDebugHandler(QtMsgType type, QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(type);
+    Q_UNUSED(context);
+    Q_UNUSED(msg);
+
+    //TODO - Implement custom debug output
+}
 
---
+++ logging/debug_handler.h
@@ -0,0 +1,8 @@
+#ifndef LOGGING_DEBUG_HANDLER_H
+#define LOGGING_DEBUG_HANDLER_H
+
+#include <QtGlobal>
+
+void AndromedaDebugHandler(QtMsgType type, QMessageLogContext &context, const QString &msg);
+
+#endif //LOGGING_DEBUG_HANDLER_H
